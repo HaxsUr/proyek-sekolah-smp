30327a6a902f321364e7e2a22e4107b5f25c9b42
Muhammad Ramadhan <133194051+HaxsUr@users.noreply.github.com>
2025-08-23 22:02:32 +0800

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
+// backend/db.js
+require('dotenv').config();
 const { Pool } = require('pg');
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString,
 });
 
 module.exports = {
